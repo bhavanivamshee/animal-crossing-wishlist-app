d27c38456e84aaef00ac184e780d25b6165669f7
Bhavani <bhavanivamshee@gmail.com>
2021-05-16 20:35:06 -0500

    updates index and show page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
 
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
     def index
         @users = User.all
     end
-    
+
+    def show
+        @user = User.find_by_id(params[:id])
+    end
+
 end
+
+private 
+
+    def find_user
+        @user = User.find_by_id(params[:id])
+    end
